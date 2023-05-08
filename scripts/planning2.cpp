e84a6bc86c2d3ec909ddd9a366978d3c501c007f
GitSRealpe <msn-realpe@hotmail.com>
2023-05-08 10:22:42 +0200

    completed planning + visual markers

--- scripts/planning2.cpp
+++ scripts/planning2.cpp
@@ -9,6 +9,9 @@
 #include <tf2_ros/transform_listener.h>
 #include <tf2_eigen/tf2_eigen.h>
 // #include <tf/transform_listener.h>
+#include <rviz_visual_tools/rviz_visual_tools.h>
+
+#include <hrov_martech2023/PlanGoal.h>
 
 // OMPL STUFF
 #include <ompl/base/SpaceInformation.h>
@@ -28,54 +31,53 @@
 namespace ob = ompl::base;
 namespace og = ompl::geometric;
 
-// fcl::CollisionObjectf tree_obj;
-std::shared_ptr<fcl::CollisionObjectf> tree_obj;
-geometry_msgs::TransformStamped t;
-
 class Validator : public ob::StateValidityChecker
 {
 public:
-    Validator(const ob::SpaceInformationPtr &si) : ob::StateValidityChecker(si)
+    Validator(const ob::SpaceInformationPtr &si, std::shared_ptr<fcl::CollisionObjectf> tree) : ob::StateValidityChecker(si) //, tree_obj_(tree)
     {
         std::cout << "validator initializing\n";
-        auv_box_ = std::shared_ptr<fcl::Boxf>(new fcl::Boxf(1, 1, 1));
-        // fcl::Transform3f fr;
+        auv_box_ = std::shared_ptr<fcl::Boxf>(new fcl::Boxf(2, 2, 2));
         auv_co_.reset(new fcl::CollisionObjectf(auv_box_));
-        // tree_obj.reset(new fcl::CollisionObjectf(auv_box_));
-        // auv_co_ = fcl::CollisionObjectf(auv_box_);
+        tree_obj_ = tree;
     }
     virtual bool isValid(const ob::State *state) const
     {
-        std::cout << "validandoo beep boop\n";
-        // si_->printState(state);
+        // std::cout << "validandoo beep boop\n";
         ob::ScopedState<> stt(si_->getStateSpace(), state);
-        stt.print();
-        // for (auto x : stt.reals())
-        // {
-        //     std::cout << x << "\n";
-        // }
+        // stt.print();
+        auv_co_->setTranslation(Eigen::Vector3f(stt.reals().at(0), stt.reals().at(1), stt.reals().at(2)));
+        auv_co_->setRotation(Eigen::AngleAxisf(stt.reals().at(3), Eigen::Vector3f::UnitZ()).matrix());
+        // std::cout << auv_co_->getTranslation() << "\n";
 
         fcl::CollisionRequestf col_req_;
         fcl::CollisionResultf col_res_;
-        fcl::Transform3f auv_tf(tf2::transformToEigen(t));
-        auv_co_->setTransform(auv_tf);
+        fcl::collide(tree_obj_.get(), auv_co_.get(), col_req_, col_res_);
 
-        std::cout << auv_co_->getTranslation() << "\n";
-        std::cout << tree_obj->getTranslation() << "\n";
-        fcl::collide(tree_obj.get(), auv_co_.get(), col_req_, col_res_);
-        std::cout << col_res_.isCollision() << "\n";
-        return true;
+        if (col_res_.isCollision())
+        {
+            // std::cout << "colision at: \n";
+            // std::cout << auv_co_->getTranslation() << "\n";
+            return false;
+        }
+        else
+        {
+            return true;
+        }
     }
 
 private:
     // fcl
     std::shared_ptr<fcl::Boxf> auv_box_;
     std::shared_ptr<fcl::CollisionObjectf> auv_co_;
-    // fcl::CollisionObjectf auv_co_;
-    // fcl::CollisionRequestf col_req_;
-    // fcl::CollisionResultf col_res_;
-    // std::shared_ptr<fcl::CollisionObjectf> tree_obj;
+    std::shared_ptr<fcl::CollisionObjectf> tree_obj_;
 };
+
+bool sendWaypoints(hrov_martech2023::PlanGoal::Request &req, hrov_martech2023::PlanGoal::Response &res)
+{
+    ROS_INFO_STREAM("Sending los waypoints " << 3);
+    return true;
+}
 
 int main(int argc, char **argv)
 {
@@ -83,25 +85,41 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "planning");
     ros::NodeHandle nh;
 
+    ros::ServiceServer service = nh.advertiseService("getWaypoints", sendWaypoints);
+
     octomap_msgs::OctomapConstPtr mapa_msg = ros::topic::waitForMessage<octomap_msgs::Octomap>("/octomap_binary");
     octomap::AbstractOcTree *abs_tree = octomap_msgs::msgToMap(*mapa_msg);
     std::shared_ptr<octomap::OcTree> octree(dynamic_cast<octomap::OcTree *>(abs_tree));
 
-    // porque especificar el type?? fcl::OcTree<float> tree(octree);
     std::shared_ptr<fcl::OcTreef> tree(new fcl::OcTreef(octree));
     std::cout << tree->getFreeThres() << "\n";
     std::shared_ptr<fcl::CollisionGeometryf> geo(tree);
-    // fcl::CollisionObjectf tree_obj(geo);
-    // std::shared_ptr<fcl::CollisionObjectf> tree_obj;
-    tree_obj.reset(new fcl::CollisionObjectf(geo));
-    // tree_obj = fcl::CollisionObjectf(geo);
+    std::shared_ptr<fcl::CollisionObjectf> tree_obj(new fcl::CollisionObjectf(geo));
 
-    std::shared_ptr<fcl::Boxf> auv_box(new fcl::Boxf(2, 2, 2));
+    auto r3(std::make_shared<ob::RealVectorStateSpace>(3));
+    r3->setName("Position");
+    ob::RealVectorBounds bounds(3);
+    bounds.setLow(0, -10);
+    bounds.setHigh(0, 10);
+    bounds.setLow(1, -10);
+    bounds.setHigh(1, 10);
+    bounds.setLow(2, -5);
+    bounds.setHigh(2, -0.2);
+    r3->setBounds(bounds);
+    auto so2(std::make_shared<ob::SO2StateSpace>());
+    so2->setName("Yaw");
+    auto navSpace = r3 + so2;
+    navSpace->setName("navSpace");
+
+    auto si(std::make_shared<ob::SpaceInformation>(navSpace));
+    si->printSettings();
+    std::cout << "\n";
+    si->setStateValidityChecker(ob::StateValidityCheckerPtr(new Validator(si, tree_obj)));
 
     // esta tf sera el start state y ya
     tf2_ros::Buffer tfBuffer;
     tf2_ros::TransformListener tfListener(tfBuffer);
-    // geometry_msgs::TransformStamped t;
+    geometry_msgs::TransformStamped t;
     while (true)
     {
         try
@@ -115,57 +133,90 @@ int main(int argc, char **argv)
             ros::Duration(0.2).sleep();
             continue;
         }
-        // ros::Duration(0.2).sleep();
     }
     std::cout << t << "\n";
-
-    tf2::transformToEigen(t);
-    fcl::Transform3f auv_tf(tf2::transformToEigen(t));
-    Eigen::IOFormat fmt(3, 0, ", ", ";\n", "[", "]", "[", "]");
-    std::cout << auv_tf.matrix().format(fmt) << "\n";
-
-    // fcl::CollisionObjectf auv_co(auv_box, auv_tf);
-    std::shared_ptr<fcl::CollisionObjectf> auv_co;
-    auv_co.reset(new fcl::CollisionObjectf(auv_box, auv_tf));
-    fcl::CollisionRequestf col_req;
-    fcl::CollisionResultf col_res;
-    // fcl::collide(tree_obj.get(), &auv_co, col_req, col_res);
-    fcl::collide(tree_obj.get(), auv_co.get(), col_req, col_res);
-    std::cout << col_res.isCollision() << "\n";
-
-    auto r3(std::make_shared<ob::RealVectorStateSpace>(3));
-    r3->setName("Position");
-    ob::RealVectorBounds bounds(3);
-    bounds.setLow(0, -10);
-    bounds.setHigh(0, 10);
-    bounds.setLow(1, -10);
-    bounds.setHigh(1, 10);
-    bounds.setLow(2, 0.5);
-    bounds.setHigh(2, 5);
-    r3->setBounds(bounds);
-    auto so2(std::make_shared<ob::SO2StateSpace>());
-    so2->setName("Yaw");
-    auto navSpace = r3 + so2;
-    navSpace->setName("navSpace");
-
-    auto si(std::make_shared<ob::SpaceInformation>(navSpace));
-    si->printSettings();
-    std::cout << "\n";
-
-    si->setStateValidityChecker(ob::StateValidityCheckerPtr(new Validator(si)));
-
-    // ob::ScopedState<> start(navSpace);
-    // start.random();
-    // start.print();
-    // si->isValid(start.get());
 
     ob::ScopedState<> current(navSpace);
     current[0] = t.transform.translation.x;
     current[1] = t.transform.translation.y;
     current[2] = t.transform.translation.z;
     current[3] = tf2::transformToEigen(t).rotation().eulerAngles(2, 1, 0)[0];
-    // current.print();
     si->isValid(current.get());
+
+    ob::ScopedState<> goal(navSpace);
+    goal[0] = 0;
+    goal[1] = 6;
+    goal[2] = -1;
+    goal[3] = 0;
+    si->isValid(goal.get());
+
+    // create a problem instance
+    auto pdef(std::make_shared<ob::ProblemDefinition>(si));
+
+    // set the start and goal states
+    pdef->setStartAndGoalStates(current, goal);
+
+    // create a planner for the defined space
+    auto planner(std::make_shared<og::RRTConnect>(si));
+
+    // set the problem we are trying to solve for the planner
+    planner->setProblemDefinition(pdef);
+
+    // perform setup steps for the planner
+    planner->setup();
+    planner->setRange(0.7);
+    // planner->setIntermediateStates(false);
+    planner->printSettings(std::cout);
+    // print the problem settings
+    std ::cout << "pdef print\n";
+    pdef->print();
+
+    // attempt to solve the problem
+    ob::PlannerStatus solved = planner->ob::Planner::solve(60.0);
+
+    if (solved)
+    {
+        std::cout << "Found solution:" << std::endl;
+
+        // For visualizing things in rviz
+        rviz_visual_tools::RvizVisualToolsPtr visual_tools_;
+        visual_tools_.reset(new rviz_visual_tools::RvizVisualTools("world", "/rviz_visual_markers"));
+        visual_tools_->deleteAllMarkers();
+
+        // path->print(std::cout);
+        og::PathGeometric pathr = *pdef->getSolutionPath()->as<og::PathGeometric>();
+        std::cout << pathr.getStateCount() << "\n";
+
+        ob::PlannerData pd(si);
+        planner->getPlannerData(pd);
+        std::cout << pd.numVertices() << std::endl;
+
+        EigenSTL::vector_Vector3d puntos;
+        Eigen::Isometry3d punto;
+        std::vector<rviz_visual_tools::colors> colorlist = {rviz_visual_tools::WHITE, rviz_visual_tools::BLUE};
+        std::vector<rviz_visual_tools::colors> colors;
+        int i = 0;
+        for (auto stt : pathr.getStates())
+        {
+            ob::ScopedState<ob::CompoundStateSpace> sstt(navSpace, stt);
+
+            punto = Eigen::AngleAxisd(sstt.reals().at(3), Eigen::Vector3d::UnitZ());
+            punto.translation().x() = sstt.reals().at(0);
+            punto.translation().y() = sstt.reals().at(1);
+            punto.translation().z() = sstt.reals().at(2);
+            puntos.push_back(punto.translation());
+            colors.push_back(colorlist.at(i++ % colorlist.size()));
+            visual_tools_->publishArrow(punto, rviz_visual_tools::RED, rviz_visual_tools::LARGE);
+        }
+
+        visual_tools_->publishPath(puntos, colors, 0.05);
+        // Don't forget to trigger the publisher!
+        visual_tools_->trigger();
+    }
+    else
+    {
+        std::cout << "No solution found" << std::endl;
+    }
 
     return 0;
 }
