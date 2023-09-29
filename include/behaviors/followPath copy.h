1e824cf578f1e3417ff362b7de589ad0b7204262
GitSRealpe <msn-realpe@hotmail.com>
2023-09-29 12:42:10 +0200

    copy of past followpath behaviour

---
+++ include/behaviors/followPath copy.h
@@ -0,0 +1,56 @@
+#ifndef BEHAVIOR_TREE_BT_FOLLOWPATH_
+#define BEHAVIOR_TREE_BT_FOLLOWPATH_
+
+#include <behaviortree_cpp_v3/action_node.h>
+#include <behaviortree_cpp_v3/bt_factory.h>
+#include <std_msgs/String.h>
+// #include <girona_utils/PathStatus.h>
+#include <girona_utils/PathAction.h>
+#include <ros/ros.h>
+#include <thread>
+
+// para registrar el nodo este
+#include <behaviors/ros_utils2.h>
+
+namespace BT
+{
+    class FollowPath : public BT::StatefulActionNode
+    {
+    public:
+        FollowPath(ros::NodeHandle &nh, const std::string &name, const BT::NodeConfiguration &conf);
+        // what is delete ?
+        FollowPath() = delete;
+
+        static PortsList providedPorts()
+        {
+            // return {InputPort<std::string>("msg_topic", "Topic of the msg")};
+            return {OutputPort<bool>("success", "Trajectory succesfull?")};
+            // return {InputPort("msg_topic", "Topic of the msg")};
+        }
+
+    protected:
+        /// method to be called at the beginning.
+        /// If it returns RUNNING, this becomes an asychronous node.
+        NodeStatus onStart() override;
+
+        /// method invoked by a RUNNING action.
+        NodeStatus onRunning() override;
+
+        /// when the method halt() is called and the action is RUNNING, this method is invoked.
+        /// This is a convenient place todo a cleanup, if needed.
+        void onHalted() override;
+
+        // why virtual, why not override like CheckAttachment.h
+        // void callback(const std_msgs::String &msg) override;
+        // virtual void callback(const std_msgs::String &msg);
+        // virtual void callback(girona_utils::PathStatusConstPtr);
+        virtual void feedback(girona_utils::PathFeedbackConstPtr);
+
+        ros::NodeHandle nh_;
+        // ros::Subscriber sub_;
+        std::string msg_topic_;
+        girona_utils::PathFeedbackConstPtr pathStatus_;
+    };
+} // namespace BT
+
+#endif // BEHAVIOR_TREE_BT_FollowPath_
---
+++ src/bt/followPathService.cpp
@@ -0,0 +1,69 @@
+#include <behaviors/followPath.h>
+#include <std_srvs/Trigger.h>
+
+ros::ServiceClient moveClient;
+
+namespace BT
+{
+    FollowPath::FollowPath(ros::NodeHandle &nh, const std::string &name, const BT::NodeConfiguration &conf)
+        : BT::StatefulActionNode(name, conf), nh_(nh)
+    {
+        std::cout << "Instance of " << name << " created!" << std::endl; // Check when the object is built
+        std::cout << "using rosnode: " << nh_.getNamespace() << "\n";
+        moveClient = nh.serviceClient<std_srvs::Trigger>("path_manager_server/startPath");
+        ros::Subscriber subFeed = nh.subscribe("path_manager_server/status", 10, &FollowPath::callback, this);
+    }
+
+    void FollowPath::callback(const girona_utils::PathStatusConstPtr pathMsg)
+    {
+        std::cout << pathMsg->status << "\n";
+    }
+
+    NodeStatus FollowPath::onStart()
+    {
+        // getInput<std::string>("pose_topic", msg_topic_).value();
+        // getInput("msg_topic", msg_topic_);
+        // std::cout << msg_topic_ << "\n";
+
+        std::cout << "follow path server is " << moveClient.exists() << "\n";
+        // ROS_INFO_STREAM(name() << " Starting to wait for msg on topic " << msg_topic_ << " yeh!");
+
+        std_srvs::Trigger req;
+        if (moveClient.call(req))
+        {
+            std::cout << "call was succesfull \n";
+            return NodeStatus::SUCCESS;
+        }
+        else
+        {
+            std::cout << "call did not succeed \n";
+            return NodeStatus::FAILURE;
+        }
+
+        return NodeStatus::RUNNING;
+    }
+    NodeStatus FollowPath::onRunning()
+    {
+        // ROS_INFO_STREAM(name() << " Waiting for user input on topic " << msg_topic_);
+
+        switch (pathStatus_.status)
+        {
+        case girona_utils::PathStatus::RUNNING:
+            return NodeStatus::RUNNING;
+            break;
+        case girona_utils::PathStatus::END_REACHED:
+            setOutput("success", true);
+            return NodeStatus::SUCCESS;
+        case girona_utils::PathStatus::STOPPED:
+            return NodeStatus::FAILURE;
+        default:
+            return NodeStatus::FAILURE;
+            break;
+        }
+    }
+    void FollowPath::onHalted()
+    {
+        ROS_INFO_STREAM(name() << " Halted!");
+    }
+
+} // namespace BT
