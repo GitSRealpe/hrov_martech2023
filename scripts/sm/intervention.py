4bb0fed539611299aff4b649f1b0b48618237d54
GitSRealpe <msn-realpe@hotmail.com>
2023-11-05 21:21:32 +0100

    starting intervention sm

--- scripts/sm/intervention.py
+++ scripts/sm/intervention.py
@@ -5,32 +5,26 @@ from statemachine import StateMachine, State
 import actionlib.simple_action_client
 
 from std_msgs.msg import String
-from hrov_martech2023.msg import BaseGoal
-from hrov_martech2023.srv import PlanGoal, PlanGoalRequest
+from optihrov2_exploration.msg import BaseGoal
+from optihrov2_exploration.srv import PlanGoal, PlanGoalRequest
+from std_srvs.srv import Trigger
 from girona_utils.msg import PathAction, PathGoal
 from nav_msgs.msg import Path
 
-value = 1
 
+class IAUVIntervention(StateMachine):
+    # states
+    start = State(initial=True)
+    auv_hovering = State()
+    unfolding = State()
+    arm_hovering = State()
+    arm_positioning= State()
+    valve_turning = State()
 
-class IAUVExploration(StateMachine):
-    "A traffic light machine"
-
-    clustering = State(initial=True)
-    planning = State()
-    following = State()
-
-    doCluster = following.to(clustering)
-    planPath = clustering.to(planning) | planning.to(planning) | following.to(planning)
-    # rePlan = planning.to(planning) | following.to(planning)
-    followPath = planning.to(following)
-
-    # cycle = (
-    #     clustering.to(planning)
-    #     | rePlan
-    #     | planning.to(following)
-    #     | following.to(clustering)
-    # )
+    # actions
+    move_auv = start.to(auv_hovering)
+    move_arm = auv_hovering.to(unfolding) | unfolding.to(arm_hovering) | arm_hovering.to(arm_positioning)
+    turn_valve=arm_positioning.to(valve_turning)
 
     def on_enter_state(self, event, state):
         print(f"Now at state '{state.id}'")
@@ -38,7 +32,11 @@ class IAUVExploration(StateMachine):
     def on_exit_state(self, event, state):
         print(f"Exiting '{state.id}' state from '{event}' event.")
 
-    def on_enter_planning(self):
+    def on_enter_auv_hovering(self):
+        clusterSrv = rospy.ServiceProxy("getClusters", Trigger)
+        clusterSrv.call()
+
+    def on_enter_unfolding(self):
         goal: BaseGoal = rospy.wait_for_message("/base_goal", BaseGoal)
         planSrv = rospy.ServiceProxy("getPath", PlanGoal)
         req = PlanGoalRequest()
@@ -46,23 +44,23 @@ class IAUVExploration(StateMachine):
         req.yaw = goal.yaw
         planSrv(req)
 
-    def on_enter_following(self):
+    def on_enter_arm_hovering(self):
         client = actionlib.SimpleActionClient("path_manager", PathAction)
-        print("before the wait")
         client.wait_for_server()
-        print("after the wait")
         # Creates a goal to send to the action server.
         goal = PathGoal()
         goal.path = rospy.wait_for_message("planner/path_result", Path)
         # Sends the goal to the action server.
         client.send_goal(goal)
+        client.wait_for_result()
+        self.send("doCluster")
 
     # def on_exit_following(self):
     #     print("Go ahead!")
 
 
-sm = IAUVExploration()
-sm._graph().write_png(os.getcwd() + "/st.png")
+sm = IAUVIntervention()
+sm._graph().write_png(os.getcwd() + "/intervention_states.png")
 
 
 def stageCB(msg: String):
@@ -79,6 +77,7 @@ def stageCB(msg: String):
 # sm.send("rePlan")
 # sm.send("followPath")
 # sm.send("rePlan")
+sm.send("doCluster")
 
 rospy.init_node("state_machine")
 rospy.Subscriber("/stage", String, stageCB, queue_size=1)
